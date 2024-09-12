9c5850bddd46e3b00b66cd803c2773ed9cce4c8c
Jenifer97 <jenifer.ayala.a@gmail.com>
2024-09-12 12:00:27 -0500

    AjusteDos

--- calculadora/src/logica/Operaciones.java
+++ calculadora/src/logica/Operaciones.java
@@ -15,12 +15,32 @@ public class Operaciones {
         this.valorDos=valorDos;
     }
      //metodos
+
+    //Suma
     public int suma(){
 
-        int suma = this.valorUno+ this.valorDos;
+        int suma = this.valorUno + this.valorDos;
         return suma;
-        
     }
+    //Resta
+    public int resta(){
+
+        int resta = this.valorUno - this.valorDos;
+        return resta;
+    }
+    //Multiplicacion
+    public int multiplicacion(){
+
+        int multiplicacion = this.valorUno * this.valorDos;
+        return multiplicacion;
+    }
+    //Divicion
+    public double divicion (){
+
+        double divicion = (double) this.valorUno/(double) this.valorDos;
+        return divicion;
+    }
+
 
 
 }
