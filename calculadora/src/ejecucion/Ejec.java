1673584a75b9f6e63724911eaf18e9f2fa018187
Jenifer97 <jenifer.ayala.a@gmail.com>
2024-09-13 10:31:08 -0500

    AjusteFinal1

--- calculadora/src/ejecucion/Ejec.java
+++ calculadora/src/ejecucion/Ejec.java
@@ -10,75 +10,98 @@ public class Ejec {
 
         Scanner scannerJen = new Scanner(System.in);
 
-        System.out.println("************************");
-        System.out.println("-  1. Suma             -");
-        System.out.println("-  2. Resta            -");
-        System.out.println("-  3. Multiplicación   -");
-        System.out.println("-  4. División         -");
-        System.out.println("-  5. Salir            -");
-        System.out.println("************************");
+        int opcion=0;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese una opción:");
-        int opciones = scanner.nextInt();
+        do {
 
-        switch (opciones) {
+            System.out.println("************************");
+            System.out.println("-  1. Suma             -");
+            System.out.println("-  2. Resta            -");
+            System.out.println("-  3. Multiplicación   -");
+            System.out.println("-  4. División         -");
+            System.out.println("-  5. Salir            -");
+            System.out.println("************************");
 
-            case 1:
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ingrese una opción:");
+            int opciones = scanner.nextInt();
 
-                System.out.println("1. Suma:");
-
-                System.out.println("Ingrese valor A:");
-                int valorUno = scannerJen.nextInt();
-                System.out.println("Ingrese valor B:");
-                int valorDos = scannerJen.nextInt();
-                Operaciones resultado = new Operaciones();
-                System.out.println("El resultado es:" + resultado.suma(valorUno, valorDos));
-
-            case 2:
-
-                System.out.println("2. Resta:");
-
-                System.out.println("Ingrese valor A:");
-                valorUno = scannerJen.nextInt();
-                System.out.println("Ingrese valor B:");
-                valorDos = scannerJen.nextInt();
-                Operaciones resultado2 = new Operaciones();
-                System.out.println("El resultado es:" + resultado2.resta(valorUno, valorDos));
-
-            case 3:
-
-                System.out.println("3. Multiplicación:");
-
-                System.out.println("Ingrese valor A:");
-                valorUno = scannerJen.nextInt();
-                System.out.println("Ingrese valor B:");
-                valorDos = scannerJen.nextInt();
-                Operaciones resultado3 = new Operaciones();
-                System.out.println("El resultado es:" + resultado3.multiplicacion(valorUno, valorDos));
-
-            case 4:
-
-                System.out.println("4. División:");
-
-                System.out.println("Ingrese valor A:");
-                valorUno = scannerJen.nextInt();
-                System.out.println("Ingrese valor B:");
-                valorDos = scannerJen.nextInt();
-                Operaciones resultado4= new Operaciones();
-                System.out.println("El resultado es:" + resultado4.division(valorUno, valorDos));
+            switch (opciones) {
 
 
+                case 1:
+
+                    System.out.println("1. Suma:");
+
+                    System.out.println("Ingrese valor A:");
+                    int valorUno = scannerJen.nextInt();
+                    System.out.println("Ingrese valor B:");
+                    int valorDos = scannerJen.nextInt();
+                    Operaciones resultado = new Operaciones();
+                    System.out.println("El resultado es:" + resultado.suma(valorUno, valorDos));
 
 
+                    scanner.nextLine(); // Captura el salto de línea restante
+                    scanner.nextLine(); // Espera a que el usuario presione Enter
+                    break;
 
 
+                case 2:
 
+                    System.out.println("2. Resta:");
 
+                    System.out.println("Ingrese valor A:");
+                    valorUno = scannerJen.nextInt();
+                    System.out.println("Ingrese valor B:");
+                    valorDos = scannerJen.nextInt();
+                    Operaciones resultado2 = new Operaciones();
+                    System.out.println("El resultado es:" + resultado2.resta(valorUno, valorDos));
+
+                    scanner.nextLine(); // Captura el salto de línea restante
+                    scanner.nextLine(); // Espera a que el usuario presione Enter
+                    break;
+
+                case 3:
+
+                    System.out.println("3. Multiplicación:");
+
+                    System.out.println("Ingrese valor A:");
+                    valorUno = scannerJen.nextInt();
+                    System.out.println("Ingrese valor B:");
+                    valorDos = scannerJen.nextInt();
+                    Operaciones resultado3 = new Operaciones();
+                    System.out.println("El resultado es:" + resultado3.multiplicacion(valorUno, valorDos));
+
+                    scanner.nextLine(); // Captura el salto de línea restante
+                    scanner.nextLine(); // Espera a que el usuario presione Enter
+                    break;
+
+                case 4:
+
+                    System.out.println("4. División:");
+
+                    System.out.println("Ingrese valor A:");
+                    valorUno = scannerJen.nextInt();
+                    System.out.println("Ingrese valor B:");
+                    valorDos = scannerJen.nextInt();
+                    Operaciones resultado4 = new Operaciones();
+                    System.out.println("El resultado es:" + resultado4.division(valorUno, valorDos));
+
+                    scanner.nextLine(); // Captura el salto de línea restante
+                    scanner.nextLine(); // Espera a que el usuario presione Enter
+                    break;
+
+                default:
+                    System.out.println("Opción Invalida");
+            }
+
+        }
+            while (opcion != 6) ;
+            System.out.println("Finalizo");
+
+        }
 
     }
-
-    }}
 
 
 
